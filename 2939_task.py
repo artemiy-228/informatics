b3f4838a427c302a0e966888ff971ea156ef3e7a
artemiy <artemsch17@gmail.com>
2021-02-23 12:04:14 +0500

    task_2943 bug fix

--- 2939_task.py
+++ 2939_task.py
@@ -1,6 +1,6 @@
 # https://informatics.msk.ru/mod/statements/view.php?id=2296&chapterid=2939#1
 
-N = int(input())
-K = int(input())
-print(K % N)
+n = int(input())
+k = int(input())
+print(k % n)
 
--- 2943_task.py
+++ 2943_task.py
@@ -1,4 +1,4 @@
 # https://informatics.msk.ru/mod/statements/view.php?id=2296&chapterid=2943#1
 
-a = abs(int(input()))
+a = int(input())
 print(a // 10 % 10)
