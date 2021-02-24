611c3437b35b7e609541e2c904586243bc542880
artemiy <artemsch17@gmail.com>
2021-02-24 11:30:34 +0500

    2952_task_1

---
+++ 2952_task.py
@@ -0,0 +1,8 @@
+# https://informatics.msk.ru/mod/statements/view.php?id=2296&chapterid=2952#1
+
+h1, m1, s1 = int(input()), int(input()), int(input())
+h2, m2, s2 = int(input()), int(input()), int(input())
+print(abs((h1 * 3600 + m1 * 60 + s1) - (h2 * 3600 + m2 * 60 + s2)))
+
+
+
---
+++ 2955_task.py
@@ -0,0 +1,9 @@
+# https://informatics.msk.ru/mod/statements/view.php?id=2296&chapterid=2955#1
+
+h = int(input())
+a = int(input())
+b = int(input())
+
+print(((h + a) // 2) + b)
+
+
