3148734bbed54b8de020038bea072595fb9283ba
artemiy <artemsch17@gmail.com>
2021-02-24 09:40:28 +0500

    2942_task

--- 2940_task.py
+++ 2940_task.py
@@ -1,11 +1,11 @@
 # https://informatics.msk.ru/mod/statements/view.php?id=2296&chapterid=2940#1
 
-h = 109
+s = 109
 v = int(input())
 t = int(input())
 
-vt = abs(v * t - h * (v * t // h))
-print(int(vt))
+vt = abs(v * t - s * (v * t // s))
+print(vt)
 
 
 
---
+++ 2942_task.py
@@ -0,0 +1,4 @@
+# https://informatics.msk.ru/mod/statements/view.php?id=2296&chapterid=2942#1
+
+a = int(input())
+print(a // 10)
