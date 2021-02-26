317adbd9c8d2a943b761abe45ad0a6dc512354ed
artemiy <artemsch17@gmail.com>
2021-02-26 20:28:06 +0500

    2950_task?

--- 2948_task.py
+++ 2948_task.py
@@ -1,12 +1,12 @@
 # https://informatics.msk.ru/mod/statements/view.php?id=2296&chapterid=2948#1
 
 n = int(input())
+h1 = (n // 86400)
 h = (n // 3600)
-k = h % 23
-h = h - k
+k = n // 24
 mm = (n % 3600) // 60
 ss = (n % 60)
 
 mm = ("0" + str(mm))[-2:]
 ss = ("0" + str(ss))[-2:]
-print(h, mm, ss, sep=":")
+print((h1 * h), mm, ss, sep=":")
---
+++ 2950_task.py
@@ -0,0 +1,7 @@
+# https://informatics.msk.ru/mod/statements/view.php?id=2296&chapterid=2950#1
+
+a = int(input())
+b = a * 45
+c = b // 60 + 9
+print(c, b // 45 + a % 2 * 5 + a % 2 * 15)
+
