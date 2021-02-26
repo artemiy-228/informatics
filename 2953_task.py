3b93482681b7996b6c13b5a7e33947689793f617
artemiy <artemsch17@gmail.com>
2021-02-26 22:14:13 +0500

    2953_task

---
+++ 2953_task.py
@@ -0,0 +1,19 @@
+# https://informatics.msk.ru/mod/statements/view.php?id=2296&chapterid=2953#1
+
+n = int(input())
+m = int(input())
+c = m + n - 1
+d = c // n
+print(d)
+
+
+# n = int(input())
+# m = int(input())
+#
+# if m % n == 0:
+#     print(m // n)
+# else:
+#     print(m // n + 1)
+
+
+
