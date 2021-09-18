2496ef121002fe96154314da371f46d49c5e3843
artemiy-228 <79500772+artemiy-228@users.noreply.github.com>
2021-09-18 21:57:15 +0500

    Add files via upload

---
+++ task№2950.py
@@ -0,0 +1,6 @@
+#https://informatics.msk.ru/mod/statements/view.php?id=2296&chapterid=2950#1
+
+lessons = ['9 45', '10 35', '11 35',  '12 25', '13 25', '14 15', '15 15', '16 05', ' 17 05', '17 55']
+
+num = int(input())
+print(lessons[num-1])
